b56365c8f615bc9323aec6c331b1a8f7d37f6aa9
Tuan Do <dotuan1990@gmail.com>
2024-09-25 08:18:12 -0700

    new file:   calculator.html 	new file:   closure.js 	new file:   test.html 	modified:   test.js

--- test.js
+++ test.js
@@ -15,3 +15,96 @@ x = "the"
 console.log(x)
 x= "222"
 console.log(x)
+x = undefined
+console.log(x)
+console.log(typeof x)
+x = null
+console.log(typeof x)
+console.log(x)
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+console.log(greet('Alice')); 
+
+const add = function(a, b) {
+  return a + b;
+};
+console.log(add(5, 3));
+
+const multiply = (x, y) => x * y;
+console.log(multiply(4, 2));
+
+(function() {
+  console.log('Hello World!');
+})();
+
+function operateOnNumbers(a, b, operation) {
+  return operation(a, b);
+}
+
+const result = operateOnNumbers(6, 3, (x, y) => x - y);
+console.log(result);
+
+//5.Nested functions
+function A(a){
+  return function B(b){
+      return function C(c){
+          return function D(d){
+              return a+b+c+d
+          }
+      }
+  }
+}
+var objA= A("Hi ")
+var objB= objA("guys! ")
+var objC= objB("My name is: ")
+var objD= objC("Windie")
+console.log(objC)
+
+
+// var newVal = "Interesting"
+// function Random(){
+//   console.log(window.newVal)
+//   var newVal = "Interested"
+//   VeryRandom()
+//   function VeryRandom(){
+
+//     console.log(newVal)
+//     var newVal = "Interested"
+//   }
+// }
+
+// Random()
+var newVal = "Interesting"; 
+function Random() {
+  console.log(newVal); //No way to access global variable
+  var newVal = ""; 
+}
+Random(); 
+
+const account = {
+  accountHolder: 'Tuan',
+};
+
+function printAccountDetails(account, callbackFunction) {
+  const message = `Account name: ${account.accountHolder}:\n`
+  callbackFunction(message);
+}
+
+function displayInfo(message) {
+  console.log(message)
+}
+printAccountDetails(account, displayInfo);
+const sessions = [
+  { time: '10:00 AM', activity: 'Team Meeting' },
+  { time: '1:00 PM', activity: 'Project Review' },
+];
+
+function displaySessions(sessions, callback) {
+  sessions.forEach(session => {
+      const sessionMessage = `Session at ${session.time}: ${session.activity}`;
+      callback(sessionMessage);
+  });
+}
+displaySessions(sessions, displayInfo);
